9fb2e01ac085ff86de5779f245827f1594a878ce
Jacob <andqso@gmail.com>
2012-09-04 22:42:00 -0700

    Small fixes to boy-girl data set, and rule extraction code. UNDOCUMENTED AND UNCHECKED---beware

--- extractor_synsem/extractor_synsem.py
+++ extractor_synsem/extractor_synsem.py
@@ -1,3 +1,13 @@
+from lib.exceptions import InvocationException
+from lib.amr.dag import Dag, NonterminalLabel
+from nltk import Tree
+import re
+from collections import defaultdict as ddict
+import itertools
+from parser.rule import Rule
+
+DEFAULT_COMPOSITION_DEPTH=3
+
 class ExtractorSynSem:
 
   def __init__(self):
@@ -5,7 +15,368 @@ class ExtractorSynSem:
 
   @classmethod
   def help(self):
-    return 'ESS help message'
+    return 'Usage: bolinas extract-synsem <nl_file> <mr_file> ' + \
+        '<alignment_file> <destination> [composition_depth=%d]' % \
+        DEFAULT_COMPOSITION_DEPTH
 
   def main(self, *args):
-    pass
+    if len(args) < 4:
+      raise InvocationException()
+    nl_path, mr_path, align_path, destination_prefix = args[:4]
+    if len(args) == 4:
+      composition_depth = DEFAULT_COMPOSITION_DEPTH
+    elif len(args) == 5:
+      composition_depth = int(args[4])
+    else:
+      raise InvocationException()
+
+    self.extract_rules_corpus(nl_path, mr_path, align_path, destination_prefix,
+        composition_depth)
+
+  ###
+
+  def extract_rules_corpus(self, nl_path, amr_path, alignment_path,
+      destination_prefix, composition_depth):
+
+    syn_f = open(nl_path)
+    sem_f = open(amr_path)
+    align_f = open(alignment_path)
+
+    n_examples = count_lines(amr_path)
+    announce_interval = n_examples / 10
+
+    examples = []
+    for example_i in range(n_examples):
+      syn_s = syn_f.readline().strip()
+      sem_s = sem_f.readline().strip()
+      align_s = align_f.readline().strip()
+
+      amr = Dag.from_string(sem_s)
+      tree = Tree(syn_s)
+      label_spans(tree)
+      align = get_alignments(align_s, amr)
+
+      examples.append((amr, tree, align))
+
+    rules = []
+    for example in examples:
+      example_rules = extract_rules(example[0], example[1], example[2],
+          composition_depth)
+      rules += example_rules
+
+    grammar = collect_counts(rules)
+
+    Rule.write_to_file(grammar, destination_prefix)
+
+###
+
+def count_lines(filename):
+  n_lines = 0
+  with open(filename) as f:
+    for line in f:
+      n_lines += 1
+  return n_lines
+
+def get_alignments(align_s, amr):
+  alignments = ddict(list)
+  align_s_parts = align_s.split()
+  for part in align_s_parts:
+    match = re.match(r'([^:]+):([^:]+:[^:]+):([^:]+)-(\d+)', part)
+    head = match.group(1)
+    label = match.group(2)
+    tail = match.group(3)
+    index = int(match.group(4))
+
+    edge_l = [e for e in amr.triples() if
+                e[0] == head and \
+                e[1] == label and \
+                e[2] == (tail,)]
+    assert len(edge_l) == 1
+    alignments[edge_l[0]].append(index)
+  return dict(alignments)
+
+def label_spans(tree, start=0):
+  end = start
+  if isinstance(tree, Tree):
+    for child in tree:
+      end = label_spans(child, end)
+    tree.span = (start, end)
+    return end
+  else:
+    return end + 1
+
+def minimal_aligned(constituents, tree_aligned):
+  minimal_constituents = []
+  for key in constituents:
+    start,end,height = key
+    if len(tree_aligned[key]) == 0:
+      continue
+    minimal = True
+    for key2 in constituents:
+      start2,end2,height2 = key2
+      if tree_aligned[key] == tree_aligned[key2] and start2 >= start and \
+          end2 <= end and height2 < height:
+        minimal = False
+        break
+    if not minimal:
+      continue
+    minimal_constituents.append(key)
+  return minimal_constituents
+
+# here be dragons
+
+def amr_reachable_h(edges, amr, predicate, expander, seen=None):
+  if seen == None:
+    seen = set()
+  for e in edges:
+    if e in seen:
+      continue
+    if not predicate(e):
+      continue
+    seen.add(e)
+    amr_reachable_h(expander(e), amr, predicate, expander, seen)
+  return seen
+
+def a_parents(edge, amr):
+  return amr.in_edges(edge[0])
+
+def a_children(edge, amr):
+  for t in edge[2]:
+    return amr.out_edges(t)
+
+def amr_reachable_forward(edges, amr, predicate):
+  return amr_reachable_h(edges, amr, predicate, lambda e: a_parents(e, amr))
+
+def amr_reachable_backward(edges, amr, predicate):
+  return amr_reachable_h(edges, amr, predicate, lambda e: a_children(e, amr))
+
+def amr_reachable_nothru_i(edge, amr, predicate, reachable, seen):
+  if edge in seen:
+    return
+  seen.add(edge)
+  if all(c in reachable for c in a_parents(edge, amr)):
+    for c in a_parents(edge, amr):
+      if all(p in reachable for p in a_children(edge, amr)):
+        amr_reachable_nothru_i(c, amr, predicate, reachable, seen)
+  if all(p in reachable for p in a_children(edge, amr)):
+    for p in a_children(edge, amr):
+      if all(c in reachable for c in a_parents(edge, amr)):
+        amr_reachable_nothru_i(p, amr, predicate, reachable, seen)
+
+def amr_reachable_nothru(edges, amr, predicate=lambda e: True):
+  forward = amr_reachable_forward(edges, amr, predicate)
+  backward = amr_reachable_backward(edges, amr, predicate)
+  reachable = forward | backward
+  seen = set()
+  for edge in edges:
+    amr_reachable_nothru_i(edge, amr, predicate, reachable, seen)
+  return seen
+
+def minimal_frontier(frontier):
+  min_frontier = []
+  for f in frontier:
+    fstart, fend = f[0].span
+    minimal = True
+    for g in frontier:
+      gstart, gend = g[0].span
+      if gstart >= fstart and gend <= fend and g[0].height() < f[0].height():
+        minimal = False
+        break
+    if minimal:
+      min_frontier.append(f)
+  return min_frontier
+
+def frontier_edges(amr, tree, alignments):
+  frontier = []
+  constituents = {}
+  if isinstance(tree, Tree):
+    for constituent in tree.subtrees():
+      key = (constituent.span[0], constituent.span[1], constituent.height())
+      assert key not in constituents
+      constituents[key] = constituent
+
+  tree_aligned = ddict(set)
+  for edge in alignments:
+    for index in alignments[edge]:
+      for key in constituents:
+        start,end,height = key
+        if start <= index < end:
+          tree_aligned[key].add(index)
+
+  aligned_constituents = minimal_aligned(constituents, tree_aligned)
+
+  for key in aligned_constituents:
+    start,end,height = key
+    constituent = constituents[key]
+    aligned_edges = [e for e in alignments if all(start <= i < end for i in
+      alignments[e])]
+    
+    if constituent == tree:
+      reachable_edges = amr.triples()
+    else:
+      reachable_edges = amr_reachable_nothru(aligned_edges, amr,
+          lambda e: e in aligned_edges or e not in alignments)
+
+    aligned_fragment = Dag.from_triples(reachable_edges)
+    if len(aligned_fragment.root_edges()) == 1:
+      frontier.append((constituent, aligned_fragment))
+
+  min_frontier = minimal_frontier(frontier)
+  min_frontier_sorted = sorted(min_frontier, key = lambda m:
+      len(list(m[0].subtrees())))
+
+  return min_frontier_sorted
+
+def collapse_constituent(tree, constituent, label):
+  return replace_constituent(tree, constituent, str(label))
+
+def replace_constituent(tree, constituent, new_constituent):
+  # We injected span, so the standard __eq__ check doesn't look for it
+  if tree == constituent and (not isinstance(tree, Tree) or tree.span ==
+      constituent.span):
+    return new_constituent
+  if not isinstance(tree, Tree):
+    return tree
+  n_tree = Tree(tree.node, [collapse_constituent(subtree, constituent,
+    new_constituent) for subtree in tree])
+  n_tree.span = tree.span
+  return n_tree
+
+def collapse_alignments(alignments, amr_fragment, new_triple):
+  new_alignments = {}
+  new_triple_alignment = []
+  for triple in alignments:
+    if triple in amr_fragment.triples():
+      new_triple_alignment += alignments[triple]
+    else:
+      new_alignments[triple] = alignments[triple]
+  new_triple_alignment = list(set(new_triple_alignment))
+  new_alignments[new_triple] = new_triple_alignment
+  return new_alignments
+
+def make_rule(frontier_pair, amr, tree, align, next_index):
+
+  constituent, amr_fragment = frontier_pair
+  outside_edges = [e for e in amr.triples() if e not in amr_fragment.triples()]
+
+  root_label = amr_fragment.root_edges()[0][1]
+  if isinstance(root_label, NonterminalLabel):
+    symbol = root_label.label
+    m = re.match(r'(.+)_(.+)_(\d+)', symbol)
+    role = m.group(1)
+  else:
+    role, concept = root_label.split(':')
+
+  external_nodes = amr.find_external_nodes(amr_fragment)
+  if len(external_nodes) == 0:
+    external_nodes = [amr_fragment.find_leaves()[0]]
+  # WARNING: destructive. Unfortunately we can't make the change any earlier.
+  # TODO why?
+  amr_fragment.external_nodes = external_nodes
+
+  symbol = '%s_%s_%d' % (role, constituent.node, len(external_nodes))
+  label = NonterminalLabel(symbol, next_index)
+
+  new_triple = (amr_fragment.roots[0], label, tuple(external_nodes))
+  new_amr = amr.collapse_fragment(amr_fragment, label)
+  assert new_triple in new_amr.triples()
+  new_tree = collapse_constituent(tree, constituent, label)
+  new_alignments = collapse_alignments(align, amr_fragment, new_triple)
+
+  rule = Rule(0, symbol, 1, amr_fragment, constituent, original_index =
+      next_index)
+
+  return rule, new_amr, new_tree, new_alignments, next_index+1
+
+def make_composed_rule(rule, cdict):
+  for label, crule in cdict.items():
+    replacement_triple_l = [e for e in rule.amr.triples() if e[1] == label]
+    assert len(replacement_triple_l) == 1
+    replacement_fragment = Dag.from_triples(replacement_triple_l)
+
+    new_amr = rule.amr.replace_fragment(replacement_fragment, crule.amr)
+    new_tree = replace_constituent(rule.parse, str(label), crule.parse)
+
+    new_rule = Rule(rule.rule_id, rule.symbol, rule.weight, new_amr, new_tree,
+        original_index = rule.original_index)
+    rule = new_rule
+  return rule
+
+def make_composed_rules(rules, max_depth):
+  composed_rules = []
+
+  for rule in rules:
+    composed_rules.append(rule)
+
+  for i in range(1, max_depth):
+    composed_rules_this_depth = []
+    for rule in rules:
+      nt_labels = [e[1] for e in rule.amr.triples() if isinstance(e[1],
+        NonterminalLabel)]
+      if len(nt_labels) == 0:
+        continue
+
+      composition_candidates = {}
+      for label in nt_labels:
+        composition_candidates[label] = []
+        for crule in composed_rules:
+          if crule.original_index != label.index:
+            continue
+          composition_candidates[label].append(crule)
+
+      comp_cand_list = []
+      label_list = []
+      for label, comp_cand in composition_candidates.items():
+        label_list.append(label)
+        comp_cand_list.append(comp_cand)
+      compositions = itertools.product(*comp_cand_list)
+      compositions = list(compositions)
+
+      for composition in compositions:
+        cdict = dict(zip(label_list, composition))
+        composed_rule = make_composed_rule(rule, cdict)
+        composed_rules_this_depth.append(composed_rule)
+
+    composed_rules += composed_rules_this_depth
+
+  return composed_rules
+
+def extract_rules(amr, tree, align, composition_depth):
+  rules = []
+  frontier = frontier_edges(amr, tree, align)
+  next_index = 0
+  while frontier:
+    rule, amr, tree, align, next_index = make_rule(frontier[0], amr, tree,
+        align, next_index)
+    rules.append(rule)
+    frontier = frontier_edges(amr, tree, align)
+
+  composed_rules = make_composed_rules(rules, composition_depth)
+  return composed_rules
+
+def collect_counts(rules):
+  rule_mapper = {}
+  rule_counter = {}
+  rule_normalizer = ddict(lambda:0.0)
+  for rule in rules:
+    rule_key = '%s:::%s:::%s' % (rule.symbol, rule.amr, rule.parse)
+    rule_key = re.sub(r'\s+', ' ', rule_key)
+    rule_key = re.sub(r'\[\d+\]', '[D]', rule_key)
+    if rule_key not in rule_mapper:
+      rule_mapper[rule_key] = rule
+      rule_counter[rule_key] = 1.0
+    else:
+      rule_counter[rule_key] += 1.0
+    rule_normalizer[rule.symbol] += 1.0
+
+  grammar = {}
+  next_id = 0
+  for key in rule_mapper:
+    rule = rule_mapper[key]
+    count = rule_counter[key]
+    norm = rule_normalizer[rule.symbol]
+    g_rule = Rule(next_id, rule.symbol, count/norm, rule.amr, rule.parse)
+    grammar[next_id] = g_rule
+    next_id += 1
+
+  return grammar
